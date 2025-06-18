d4722e3cea7fd99c2458c5a4526fd389725f7fcf
shufito <jshufito@gmail.com>
2025-06-18 11:03:13 -0300

    adicionado os codprods fixos 2024500036 ,2024500035, 2024300005

--- src/hook/getProdutos.tsx
+++ src/hook/getProdutos.tsx
@@ -6,7 +6,7 @@ interface IResponse {
 }
 const fetchProdutos = async (): Promise<IResponse[]> => {
   const response = await JX.consultar(
-    "SELECT CODPROD, DESCRPROD  FROM TGFPRO WHERE DESCRPROD LIKE 'ROT %' OR DESCRPROD LIKE 'CART %'"
+    "SELECT CODPROD, DESCRPROD  FROM TGFPRO WHERE DESCRPROD LIKE 'ROT %' OR DESCRPROD LIKE 'CART %' OR CODPROD IN (2024500036 ,2024500035,2024300005)"
   );
 
   return response;
